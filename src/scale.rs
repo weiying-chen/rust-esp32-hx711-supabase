1a841df57187ad45f81a741c19750a8dcd17d206
Wei-ying Chen <weiying.alex.chen@gmail.com>
2024-03-30 23:17:50 +0800

    Remove unnecessary code

--- src/scale.rs
+++ src/scale.rs
@@ -1,8 +1,6 @@
-use std::collections::VecDeque;
-
 use anyhow::Result;
 use esp_idf_svc::hal::{
-    delay::{Ets, FreeRtos},
+    delay::Ets,
     gpio::{Input, InputPin, Output, OutputPin, Pin, PinDriver},
     peripheral::Peripheral,
 };
@@ -11,9 +9,6 @@ use loadcell::{
     LoadCell,
 };
 
-const LOADCELL_STABLE_READINGS: usize = 10; // Number of readings to consider for stability
-const LOADCELL_LOOP_DELAY_US: u32 = 10000; // Delay between readings in microseconds
-                                           // const LOADCELL_READY_DELAY_US: u32 = 1000;
 pub type LoadSensor<'a, SckPin, DtPin> =
     HX711<PinDriver<'a, SckPin, Output>, PinDriver<'a, DtPin, Input>, Ets>;
 
@@ -45,21 +40,9 @@ where
         self.load_sensor.is_ready()
     }
 
-    // fn set_scale(&mut self, scale: f32) {
-    //     self.load_sensor.set_scale(scale);
-    // }
-
     pub fn tare(&mut self, times: usize) {
         self.load_sensor.tare(times);
     }
-
-    // pub fn read_scaled(&mut self) -> Result<f32, NotReadyError> {
-    //     self.load_sensor.read_scaled()
-    // }
-
-    // fn read(&mut self) -> Result<i32, NotReadyError> {
-    //     self.load_sensor.read()
-    // }
 
     pub fn read_rounded(&mut self) -> Result<i32, NotReadyError> {
         let reading = self.load_sensor.read_scaled()?;
@@ -71,36 +54,5 @@ where
         };
 
         Ok(rounded_reading)
-    }
-
-    pub fn wait_stable(&mut self) {
-        let mut readings: VecDeque<f32> = VecDeque::with_capacity(LOADCELL_STABLE_READINGS);
-
-        loop {
-            while !self.load_sensor.is_ready() {
-                FreeRtos::delay_ms(10);
-            }
-
-            let reading = self
-                .load_sensor
-                .read_scaled()
-                .expect("Failed to read scale");
-
-            log::info!("Waiting for stable weight: {:.4}", reading);
-
-            if readings.len() == LOADCELL_STABLE_READINGS {
-                readings.pop_front();
-            }
-
-            readings.push_back(reading);
-
-            if readings.len() == LOADCELL_STABLE_READINGS
-                && readings.iter().all(|&x| (x - reading).abs() < 0.2)
-            {
-                break;
-            }
-
-            Ets::delay_us(LOADCELL_LOOP_DELAY_US);
-        }
     }
 }
